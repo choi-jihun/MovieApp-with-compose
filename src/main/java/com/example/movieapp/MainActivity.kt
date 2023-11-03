14c331de86e7fd5879b9b237464062834c617f56
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-11-03 22:02:53 +0900

    @AndroidEntryPoint 추가

--- src/main/java/com/example/movieapp/MainActivity.kt
+++ src/main/java/com/example/movieapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.movieapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
 //ComponentActivity는 jetpackcompose를 사용
 //AppCompatActivity는 xml사용
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
