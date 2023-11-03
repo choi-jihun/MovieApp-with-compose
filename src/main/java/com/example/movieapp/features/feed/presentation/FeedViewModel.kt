747f765543ad1a533ef9f725c6927df95bb103be
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-11-03 22:05:49 +0900

    파일 위치 변경

--- src/main/java/com/example/movieapp/features/feed/presentation/FeedViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.movieapp.features.feed.presentation
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-
-@HiltViewModel
-class FeedViewModel(): ViewModel() {
-}
