54e7ba5ae8b1b0ec300ea33d5fe0004194c37d8b
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-11-03 20:31:37 +0900

    주석 추가

--- src/main/java/com/example/movieapp/App.kt
+++ src/main/java/com/example/movieapp/App.kt
@@ -1,11 +1,14 @@
 package com.example.movieapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class App:Application() {
     override fun onCreate() {
         super.onCreate()
+        //Log라이브러리
         Timber.plant(Timber.DebugTree())
     }
 }
